a9d0743ad90978413bd4ec71e79948ac10d7c538
Sergey <sergioxl555@mail.ru>
2020-10-13 14:43:46 +0600

    dev::DEV-02::Logging has been added

--- src/main/java/com/service/impl/AnimalTracking.java
+++ src/main/java/com/service/impl/AnimalTracking.java
@@ -6,14 +6,16 @@ import com.exception.AnimalNotFoundException;
 import com.rules.Rule;
 import com.service.TrackingService;
 import com.util.FileUtil;
-
+import org.apache.log4j.Logger;
 import java.util.List;
 
 public class AnimalTracking implements TrackingService {
 
+    final static Logger logger = Logger.getLogger(AnimalTracking.class);
+
     private final List<Animal> animals = FileUtil.loadAnimalsFromFile(FileUtil.DEFAULT_ANIMALS_FILE_NAME);
     private final List<Rule> rules = FileUtil.loadRulesFromFile(FileUtil.DEFAULT_RULE_FILE_NAME);
-    private final String result="";
+    private final String result = "";
 
     @Override
     public void calculateAnimalsCount() throws AnimalCriteriaError, AnimalNotFoundException {
